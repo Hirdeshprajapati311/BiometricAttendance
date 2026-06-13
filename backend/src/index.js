bc628b98b1ecd2f3956e388f645e3717aac8f019
Hirdesh Prajapati <134317588+Hirdeshprajapati311@users.noreply.github.com>
2026-06-13 22:00:11 +0530

    initial commit: project setup with frontend and backend

---
+++ backend/src/index.js
@@ -0,0 +1,14 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import { connectDB } from "./config/db.js";
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+connectDB();
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
+});
