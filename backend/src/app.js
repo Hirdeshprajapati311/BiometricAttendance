3c646d78401604a7ad39bf065adec8365da501f8
Hirdesh (Tech Jose) <hirdesh.prajapati@techjose.com>
2026-07-11 12:52:42 +0530

    frontend support

--- backend/src/app.js
+++ backend/src/app.js
@@ -23,7 +23,10 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: [
+      "http://localhost:3000",
+      "http://biometric-attendance-three.vercel.app",
+    ],
     credentials: true,
   }),
 );
